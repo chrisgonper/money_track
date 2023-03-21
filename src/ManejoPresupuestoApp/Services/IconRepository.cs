b54557be4ed546ccb10b9e98e5610ba3a14ad57e
chrisgonper <chrizgabz9@gmail.com>
2023-03-20 20:29:40 -0600

    getting the conn string in the icon repo

--- src/ManejoPresupuestoApp/Services/IconRepository.cs
+++ src/ManejoPresupuestoApp/Services/IconRepository.cs
@@ -4,6 +4,12 @@ namespace ManejoPresupuestoApp.Services
 {
     public class IconRepository : IIconRepository
     {
+        private readonly string _connectionString;
+
+        public IconRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
         public Task<IEnumerable<IconModel>> GetAll()
         {
             throw new NotImplementedException();
