b0f6729d30db29228e9a4ad441839ca6f7544722
chrisgonper <chrizgabz9@gmail.com>
2023-03-20 20:24:17 -0600

    adding interface for icons

---
+++ src/ManejoPresupuestoApp/Services/IIconRepository.cs
@@ -0,0 +1,9 @@
+﻿using ManejoPresupuestoApp.Models;
+
+namespace ManejoPresupuestoApp.Services
+{
+    public interface IIconRepository
+    {
+        Task<IEnumerable<IconModel>> GetAll();
+    }
+}
---
+++ src/ManejoPresupuestoApp/Services/IconRepository.cs
@@ -0,0 +1,12 @@
+﻿using ManejoPresupuestoApp.Models;
+
+namespace ManejoPresupuestoApp.Services
+{
+    public class IconRepository : IIconRepository
+    {
+        public Task<IEnumerable<IconModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
